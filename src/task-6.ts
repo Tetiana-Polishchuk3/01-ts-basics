1c1f3ca0819112489c29b6fb2b13ba7d6b50de05
Tatiana Polishchuk <antatana98@gmail.com>
2025-05-18 17:16:54 +0300

    first commit

---
+++ src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const num = getFirstElement<number>([1, 2, 3]);
+const str = getFirstElement<string>(["a", "b", "c"]);
+const bool = getFirstElement<boolean>([true, false, true]);
+
+const mixed = getFirstElement<number | string | boolean>([1, "two", true]);
